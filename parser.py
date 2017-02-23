35764bd7ae2c3db20c439b1bb86440d8afb95c0d
Simon Gielen <simong@ziggo.nl>
2017-02-23 20:43:03 +0000

    fixed parser

--- parser.py
+++ parser.py
@@ -4,37 +4,39 @@ from stream import Endpoint
 
 def parse(fileName):
 	with open(fileName, 'r') as f:
-		lines = map(lambda line: map(int, line.split(" ")), f.readlines())
+		lines = list(map(lambda line: list(map(int, line.split(" "))), f.readlines()))
 	numberOfVideos = lines[0][0]
 	numberOfEndpoints = lines[0][1]
 	numberOfVideoRequests = lines[0][2]
 	numberOfCacheServers = lines[0][3]
 	cacheServerSize = lines[0][4]
 	videosSizes = lines[1]
+	
+	cacheList = []
+	for i in range(numberOfCacheServers):
+		cacheList.append(Cache(i, cacheServerSize))
 
 	endpoints = []
 	startIndex = 2
 	for i in range(numberOfEndpoints):
-		endpoints.append(parseEndpoint(lines, startIndex))
+		endpoints.append(parseEndpoint(lines, startIndex, cacheList))
 		startIndex += len(endpoints[i][1]) + 1
+
 	for _ in range(numberOfVideoRequests):
 		curLine = lines[startIndex]
 		startIndex += 1
-		endpoints[curLine[1]][2][curLine[0]] = Video(curLine[0], curLine[2])
+		endpoints[curLine[1]][2][Video(curLine[0], curLine[2])] = curLine[2] 
 
-	cacheList = []
 	endpointList = []
-	for i in range(numberOfCacheServers):
-		cacheList.append(Cache(i, cacheSize))
 
 	for i in range(numberOfEndpoints):
 		endpointList.append(Endpoint(i, endpoints[i][1], endpoints[i][2], endpoints[i][0]))
 	
-
+	print(endpoints)
 	return (cacheList, endpointList)
 
 
-def parseEndpoint(lines, startIndex):
+def parseEndpoint(lines, startIndex, caches):
 	returnValue = []
 
 	curLine = lines[startIndex]
@@ -47,8 +49,7 @@ def parseEndpoint(lines, startIndex):
 	for _ in range(numberOfCaches):
 		curLine = lines[startIndex]
 		startIndex += 1
-		returnValue[1][curLine[0]] = curLine[1]
+		returnValue[1][caches[curLine[0]]] = curLine[1]
 	returnValue.append({})
 
-	print(returnValue)
 	return returnValue
