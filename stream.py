735c09340addd398d50df6973213d53ea1624970
Henry Mercer <henry.mercer@me.com>
2017-02-23 18:47:56 +0000

    Add weight method

--- stream.py
+++ stream.py
@@ -5,30 +5,50 @@ class CacheServer:
 
 	videos = []
 
-	def __init__(uid, size):
-		this.uid = uid
-		this.capacity = capacity
+	def __init__(self, uid, capacity):
+		self.uid = uid
+		self.capacity = capacity
 
 class Endpoint:
 	uid = 0
-	cache_latencies = {}
-	video_requests = {} # uid, num_requests
+	cache_latencies = {} # cache uid, latency.  You probably want delta_latencies
+	delta_latencies = {} # cache uid, delta latency
+	video_requests = {} # video uid, num_requests
 	datacenter_latency = 0
 
-	def __init__(uid, cache_latencies):
-		this.uid = uid
-		this.cache_latencies = cache_latencies
+	def __init__(self, uid, cache_latencies, video_requests, datacenter_latency):
+		self.uid = uid
+		self.cache_latencies = cache_latencies
+		self.video_requests = video_requests
+		self.datacenter_latency = datacenter_latency
+
+		self.compute_delta_latencies()
+
+	def compute_delta_latencies(self):
+		for uid, cache_latency in self.cache_latencies.items():
+			self.delta_latencies[uid] = self.datacenter_latency - cache_latency
+
 
 class Video:
 	uid = 0
 	size = 0
 
-	def __init__(uid, size):
-		this.uid = uid
-		this.size = size
+	def __init__(self, uid, size):
+		self.uid = uid
+		self.size = size
 
 def weight(endpoint, cache, video):
-	pass
+	# num_requests * (datacenter latency - cache latency) / video_size
 
-def get_priority_queue():
-	pass
+	num_requests = endpoint.video_requests[video.uid]
+	delta_latency = endpoint.delta_latencies[cache.uid]
+	video_size = video.size
+
+	return num_requests * delta_latency / video_size
+
+sample_cache = CacheServer(1, 1000)
+sample_endpoint = Endpoint(1, {1: 100}, {1: 50}, 500)
+sample_video = Video(1, 500)
+
+print(weight(sample_endpoint, sample_cache, sample_video))
+
